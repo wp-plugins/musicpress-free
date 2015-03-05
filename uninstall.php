76a190dd6d6c4f5ecc8c63af2aec970b5936b600
Jesse Cohen <Jesse Cohen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-05 20:19:57 +0000

    Uninstall procedure for gigs, tracks and fans finalised
    git-svn-id: https://plugins.svn.wordpress.org/musicpress-free/tags/1.0@1106334 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -17,3 +17,59 @@ delete_option( 'musicpress_email' );
 // Remove Widgets options
 delete_option( 'mup_gig_list_widget_length' );
 delete_option( 'mup_track_list_widget_length' );
+
+global $wpdb;
+
+/**
+ * Remove all gigs from the database
+ * @since 1.0
+ */
+$cptName_gigs = 'gig';
+
+$tablePostMeta_gigs = $wpdb->prefix . 'postmeta';
+$tablePosts_gigs = $wpdb->prefix . 'posts';
+
+$postMetaDeleteQuery_gigs = "DELETE FROM $tablePostMeta_gigs".
+                      " WHERE post_id IN".
+                      " (SELECT id FROM $tablePosts_gigs WHERE post_type='$cptName_gigs'";
+
+$postDeleteQuery_gigs = "DELETE FROM $tablePosts_gigs WHERE post_type='$cptName_gigs'";
+
+$wpdb->query($postMetaDeleteQuery_gigs);
+$wpdb->query($postDeleteQuery_gigs);
+
+/**
+ * Remove all tracks from the database
+ * @since 1.0
+ */
+$cptName_tracks = 'track';
+
+$tablePostMeta_tracks = $wpdb->prefix . 'postmeta';
+$tablePosts_tracks = $wpdb->prefix . 'posts';
+
+$postMetaDeleteQuery_tracks = "DELETE FROM $tablePostMeta_tracks".
+                      " WHERE post_id IN".
+                      " (SELECT id FROM $tablePosts_tracks WHERE post_type='$cptName_tracks'";
+
+$postDeleteQuery_tracks = "DELETE FROM $tablePosts_tracks WHERE post_type='$cptName_tracks'";
+
+$wpdb->query($postMetaDeleteQuery_tracks);
+$wpdb->query($postDeleteQuery_tracks);
+
+/**
+ * Remove all fans from the database
+ * @since 1.0
+ */
+$cptName_fans = 'fan';
+
+$tablePostMeta_fans = $wpdb->prefix . 'postmeta';
+$tablePosts_fans = $wpdb->prefix . 'posts';
+
+$postMetaDeleteQuery_fans = "DELETE FROM $tablePostMeta_fans".
+                      " WHERE post_id IN".
+                      " (SELECT id FROM $tablePosts_fans WHERE post_type='$cptName_fans'";
+
+$postDeleteQuery_fans = "DELETE FROM $tablePosts_fans WHERE post_type='$cptName_fans'";
+
+$wpdb->query($postMetaDeleteQuery_fans);
+$wpdb->query($postDeleteQuery_fans);
